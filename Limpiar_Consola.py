34d78a7bf629999023bd5c253be5e6593570d042
pythonTest <vgpdata@gmail.com>
2025-04-10 10:19:56 -0600

    Subir archivo e Python

---
+++ Limpiar_Consola.py
@@ -0,0 +1,15 @@
+from os import system
+
+
+
+
+nombre = input("Dime tu Nombre: ")
+edad = input ("Dime tu edad: ")
+system("cls")
+
+print(f"Tu nombre es {nombre} y tienes {edad} anios.")
+
+
+# metodos = funciones
+
+
